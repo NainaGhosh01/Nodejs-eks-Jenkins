ec5b384b73e4116dfc9524a209a5e4caab621c78
NainaGhosh01 <nainaghosh01@gmail.com>
2025-06-20 00:29:51 +0530

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,7 @@ const app = require('../app'); // Don't call app.listen inside app.js
 describe('GET /', () => {
   it('responds with welcome message', async () => {
     const res = await request(app).get('/');
-    expect(res.text).toBe('Welcome to the Node.js App');
+    expect(res.text).toBe('Welcome to EKS Node.js App 🚀')
     expect(res.statusCode).toBe(200);
   });
 });
